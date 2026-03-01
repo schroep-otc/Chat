fb2c731b06e8c908a47270756f2796d9a8536299
SCHROEPPEL, STEPHEN C. <ss0102429@otc.edu>
2026-02-28 21:11:35 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
